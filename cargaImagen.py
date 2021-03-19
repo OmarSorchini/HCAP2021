8ea6343e59fc8a2780e726ca30410573c2151b82
OmarSorchini <omarsorchinijr@gmail.com>
2021-03-19 07:48:35 -0600

    procesar imagenes

---
+++ cargaImagen.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+#Cargar la imagen a color
+IRGB=cv2.imread('halo.jpg')
+print(IRGB)
+print(IRGB.shape)
+print('Lineas agregadas en rama 2')
+IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS)
+print(IGS.shape)
